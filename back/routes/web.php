373dd3caf70cc20b61bb3ea81ecfd5fe92e8cd5e
Aleksandr Usov <usov.als579@gmail.com>
2023-07-10 17:09:53 +0700

    #3765 fix: подмена маршрута livewire.upload-file

--- back/routes/web.php
+++ back/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Livewire\Controllers\FileUploadHandler;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/cp/livewire/upload-file', [FileUploadHandler::class, 'handle'])
+    ->name('livewire.upload-file')
+    ->middleware(config('livewire.middleware_group', ''));
