d450bb9cacb7e528d22c4a683b504eeca2099e3a
Aleksandr Usov <usov.als579@gmail.com>
2023-07-16 14:46:47 +0700

    #3778 feat: API для корзины

---
+++ back/app/Http/Controllers/Api/OrderController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\OrderRequest;
+use App\Services\OrderService;
+
+class OrderController extends Controller
+{
+    public function store(OrderRequest $request, OrderService $orderService)
+    {
+        $orderService->storeOrderInfo($request);
+    }
+
+
+}
