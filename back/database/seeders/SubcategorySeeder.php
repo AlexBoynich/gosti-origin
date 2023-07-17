4b58de10b90c6634d5ee4e2b13e62849ec556f20
Aleksandr Usov <usov.als579@gmail.com>
2023-07-17 19:13:50 +0700

    #3746 fix: Исправление сидера подкатегорий

--- back/database/seeders/SubcategorySeeder.php
+++ back/database/seeders/SubcategorySeeder.php
@@ -16,64 +16,64 @@ class SubcategorySeeder extends Seeder
         Subcategory::query()->firstOrCreate(
             [
                 'title' => 'Каши',
+                'category_id' => 1,
             ],
             [
                 'priority' => 1,
-                'category_id' => 1,
             ]
         );
         Subcategory::query()->firstOrCreate(
             [
                 'title' => 'Драник',
+                'category_id' => 1,
             ],
             [
                 'priority' => 1,
-                'category_id' => 1,
             ]
         );
         Subcategory::query()->firstOrCreate(
             [
                 'title' => 'Сырники',
+                'category_id' => 1,
             ],
             [
                 'priority' => 1,
-                'category_id' => 1,
             ]
         );
         Subcategory::query()->firstOrCreate(
             [
                 'title' => 'Блюда из яиц',
+                'category_id' => 1,
             ],
             [
                 'priority' => 1,
-                'category_id' => 1,
             ]
         );
         Subcategory::query()->firstOrCreate(
             [
                 'title' => 'Блины',
+                'category_id' => 1,
             ],
             [
                 'priority' => 1,
-                'category_id' => 1,
             ]
         );
         Subcategory::query()->firstOrCreate(
             [
                 'title' => 'Тосты и сендвичи',
+                'category_id' => 1,
             ],
             [
                 'priority' => 1,
-                'category_id' => 1,
             ]
         );
         Subcategory::query()->firstOrCreate(
             [
                 'title' => 'Сборные завтраки',
+                'category_id' => 1,
             ],
             [
                 'priority' => 1,
-                'category_id' => 1,
             ]
         );
 
@@ -81,19 +81,19 @@ class SubcategorySeeder extends Seeder
         Subcategory::query()->firstOrCreate(
             [
                 'title' => 'Приготовленное',
+                'category_id' => 4,
             ],
             [
                 'priority' => 1,
-                'category_id' => 4,
             ]
         );
         Subcategory::query()->firstOrCreate(
             [
                 'title' => 'Из заморозки',
+                'category_id' => 4,
             ],
             [
                 'priority' => 1,
-                'category_id' => 4,
             ]
         );
 
@@ -101,64 +101,64 @@ class SubcategorySeeder extends Seeder
         Subcategory::query()->firstOrCreate(
             [
                 'title' => 'Салаты',
+                'category_id' => 3,
             ],
             [
                 'priority' => 1,
-                'category_id' => 3,
             ]
         );
         Subcategory::query()->firstOrCreate(
             [
                 'title' => 'Закуски',
+                'category_id' => 3,
             ],
             [
                 'priority' => 1,
-                'category_id' => 3,
             ]
         );
         Subcategory::query()->firstOrCreate(
             [
                 'title' => 'Вторые блюда',
+                'category_id' => 3,
             ],
             [
                 'priority' => 1,
-                'category_id' => 3,
             ]
         );
         Subcategory::query()->firstOrCreate(
             [
                 'title' => 'Супы',
+                'category_id' => 3,
             ],
             [
                 'priority' => 1,
-                'category_id' => 3,
             ]
         );
         Subcategory::query()->firstOrCreate(
             [
                 'title' => 'Гарниры',
+                'category_id' => 3,
             ],
             [
                 'priority' => 1,
-                'category_id' => 3,
             ]
         );
         Subcategory::query()->firstOrCreate(
             [
                 'title' => 'Детское меню',
+                'category_id' => 3,
             ],
             [
                 'priority' => 1,
-                'category_id' => 3,
             ]
         );
         Subcategory::query()->firstOrCreate(
             [
                 'title' => 'Добавки',
+                'category_id' => 3,
             ],
             [
                 'priority' => 1,
-                'category_id' => 3,
             ]
         );
 
@@ -166,28 +166,30 @@ class SubcategorySeeder extends Seeder
         Subcategory::query()->firstOrCreate(
             [
                 'title' => 'Салаты',
+                'category_id' => 2,
             ],
             [
                 'priority' => 1,
-                'category_id' => 2,
             ]
         );
         Subcategory::query()->firstOrCreate(
             [
                 'title' => 'Супы',
+                'category_id' => 2,
+
             ],
             [
                 'priority' => 1,
-                'category_id' => 2,
             ]
         );
         Subcategory::query()->firstOrCreate(
             [
                 'title' => 'Горячее',
+                'category_id' => 2,
+
             ],
             [
                 'priority' => 1,
-                'category_id' => 2,
             ]
         );
     }
