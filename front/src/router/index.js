ed11c60e98ea50a88e39c5e91608a90a5069dfb2
YAtr1ck192 <tolya.trick@bk.ru>
2023-07-19 13:58:08 +0700

    #2414 feat: добавил динамическое изменение description'а

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -7,27 +7,47 @@ Vue.use(VueRouter)
 const routes = [
     {
         path: '/',
-        name: 'Гости',
+        name: 'home',
+        meta: {
+            title: 'Гости',
+            description: 'Городское кафе для завтраков, ланчей, ужинов с бокалом вина'
+        },
         component: HomeView,
     },
     {
         path: '/catalog',
-        name: 'Гости • Каталог',
+        name: 'catalog',
+        meta: {
+            title: 'Гости • Каталог',
+            description: 'Заказать любимые блюда домой',
+        },
         component: () => import( '../views/catalog/catalogView.vue')
     },
     {
         path: '/catering',
-        name: 'Гости • Кейтеринг',
+        name: 'catering',
+        meta: {
+            title: 'Гости • Кейтеринг',
+            description: 'Меню кейтеринга для вашего праздника или мероприятия',
+        },
         component: () => import( '../views/catering/cateringView.vue')
     },
     {
         path: '/cart',
-        name: 'Гости • Корзина',
+        name: 'cart',
+        meta: {
+            title: 'Гости • Корзина',
+            description: 'Доставка и самовывоз любимых блюд'
+        },
         component: () => import( '../views/cart/cartView.vue')
     },
     {
         path: '*',
-        name: 'Гости • 404',
+        name: '404',
+        meta: {
+            title: 'Гости • 404',
+            description: 'Страница не найдена'
+        },
         component: () => import( '../views/404/PageNotFound.vue')
     },
 ]
@@ -38,7 +58,8 @@ const router = new VueRouter({
     routes
 })
 router.beforeEach((to, from, next) => {
-    document.title = to.name;
+    document.title = to.meta.title;
+    document.querySelector('meta[name="description"]').setAttribute('content', to.meta.description);
     next();
 })
 
