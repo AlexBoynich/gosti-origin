d1b75b3341660a700c84ac64a9981ea31381f843
Aleksandr Usov <usov.als579@gmail.com>
2023-07-25 20:03:14 +0700

    #2445 fix: Удаление команды truncate при срабатывание сидеров

--- back/database/seeders/DatabaseSeeder.php
+++ back/database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 
 class DatabaseSeeder extends Seeder
@@ -13,18 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('obtaining_method_orders')->truncate();
-        DB::table('addresses')->truncate();
-        DB::table('carts')->truncate();
-        DB::table('orders')->truncate();
-        DB::table('customers')->truncate();
-        DB::table('dishes')->truncate();
-
-        DB::table('blocks')->truncate();
-        DB::table('contacts')->truncate();
-        DB::table('settings')->truncate();
-        DB::table('users')->truncate();
-
         $this->call([
             UserSeeder::class,
             CategorySeeder::class,
