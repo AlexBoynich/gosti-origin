36b9a7d274af01ce45e68a08971b9a66ef97817a
Aleksandr Usov <usov.als579@gmail.com>
2023-07-31 13:25:03 +0700

    "#0000 revert: Откат тестовой загрузки файлов"
    This reverts commit 9ce1d8b5d92e99dad4ae01de77e5e25e0866f63e.

--- back/resources/views/uploadFile.blade.php
+++
@@ -1,8 +0,0 @@
-<form
-        method="POST"
-        enctype="multipart/form-data"
-        action="{{ route('cp.store') }}">
-    @csrf
-    <input type="file" name="image"><br>
-    <input type="submit" title="Сохранить">
-</form>
--- back/routes/web.php
+++ back/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Filament\File\FileUploadHandler;
-use App\Http\Controllers\UploadFileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,6 +21,3 @@ Route::get('/', function () {
 Route::post('/cp/livewire/upload-file', [FileUploadHandler::class, 'handle'])
     ->name('livewire.upload-file')
     ->middleware(config('livewire.middleware_group', ''));
-
-Route::get('/cp/upload-file', [UploadFileController::class, 'create'])->name('cp.upload-file');
-Route::post('/cp/store', [UploadFileController::class, 'store'])->name('cp.store');
